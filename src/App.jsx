a95aff88d72f55388825b2f9aad45794bd3e8c1f
MunaAbusamra <munaabusamra0@gmail.com>
2026-05-15 09:18:41 +0300

    Add admin pages with Bootstrap (FinancialReports.jsx,SystemSettings.jsx,UserManagement.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,31 @@ import Catalog from "./pages/Catalog";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
 import BookDetails from "./pages/Bookdetails";
-function App() {
-  return (
-    <BrowserRouter>
-    <ScrollToTop />
-      <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/catalog" element={<Catalog />} />
-        <Route path="/book/:id" element={<BookDetails />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+import UserManagement from "./pages/UserManagement";
+import FinancialReports from "./pages/FinancialReports";
+import SystemSettings from "./pages/SystemSettings";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <ScrollToTop />
+            <Navbar />
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/catalog" element={<Catalog />} />
+                <Route path="/book/:id" element={<BookDetails />} />
+
+                {/* admin pages-muna */}
+                <Route path="/users" element={<UserManagement />} />
+                <Route path="/financial" element={<FinancialReports />} />
+                <Route path="/settings" element={<SystemSettings />} />
+            </Routes>
+
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
 export default App;
